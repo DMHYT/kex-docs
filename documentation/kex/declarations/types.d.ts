82168d5f8eff7a868c3bebcf3feb63ff9e8039f8
vsdum <danmarkhunter13072006@gmail.com>
2022-07-26 23:58:38 +0300

    New types for the LootModule

--- documentation/kex/declarations/types.d.ts
+++ documentation/kex/declarations/types.d.ts
@@ -32,3 +32,69 @@ declare interface INativeInterface extends java.lang.Object {
  * - The [[KEX.Dimension]] object for the needed dimension
  */
 declare type WorldOrDimension = number | BlockSource | KEX.Dimension;
+
+
+/**
+ * Object used in [[KEX.LootModule]] to specify some quantity ranges,
+ * like item count, data/damage, number of loot pool rolls etc.
+ */
+declare type MinMax = { min: number, max: number };
+
+
+/**
+ * Type alias listing all the vanilla structure string types.
+ * Used in `"exploration_map"` loot table entry function's JSON description.
+ */
+declare type FeatureTypes =
+    "buriedtreasure" |
+    "endcity" |
+    "fortress" |
+    "mansion" |
+    "mineshaft" |
+    "monument" |
+    "pillageroutpost" |
+    "ruins" |
+    "shipwreck" |
+    "stronghold" |
+    "temple" |
+    "village";
+
+
+/**
+ * Type alias listing all the vanilla enchantment string types.
+ * Used in `"specific_enchants"` loot table entry function's JSON description.
+ */
+declare type EnchantTypes =
+    "protection" |
+    "fire_protection" |
+    "feather_falling" |
+    "blast_protection" |
+    "projectile_protection" |
+    "thorns" |
+    "respiration" |
+    "aqua_affinity" |
+    "depth_strider" |
+    "sharpness" |
+    "smite" |
+    "bane_of_arthropods" |
+    "knockback" |
+    "fire_aspect" |
+    "looting" |
+    "efficiency" |
+    "silk_touch" |
+    "unbreaking" |
+    "fortune" |
+    "power" |
+    "punch" |
+    "flame" |
+    "infinity" |
+    "luck_of_the_sea" |
+    "lure" |
+    "frost_walker" |
+    "mending" |
+    "binding_curse" |
+    "vanishing_curse" |
+    "impaling" |
+    "riptide" |
+    "loyalty" |
+    "channeling";
