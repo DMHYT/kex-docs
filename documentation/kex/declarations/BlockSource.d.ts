10ec63b42cd98b6a1f39e3e08484b24629ee90bd
vsdum <danmarkhunter13072006@gmail.com>
2022-07-26 23:59:14 +0300

    BlockSource

---
+++ documentation/kex/declarations/BlockSource.d.ts
@@ -0,0 +1,77 @@
+/**
+ * Type alias for the InnerCore's [[BlockSource]]
+ * to fix names conflicting in [[KEX.BlockSource]] declaration.
+ */
+type InnerCoreBlockSource = BlockSource;
+
+declare module KEX {
+
+    /**
+     * Class extending [[InnerCoreBlockSource]]
+     * and adding some new methods not implemented in InnerCore API.
+     * @since 3.0
+     */
+    export class BlockSource extends globalThis.InnerCoreBlockSource {
+
+        /**
+         * Constructs new [[BlockSource]] object from an existing [[InnerCoreBlockSource]] object.
+         * @since 3.0
+         */
+        constructor(icObj: InnerCoreBlockSource);
+
+        /**
+         * Converts given InnerCore's `BlockSource` object to KEX `BlockSource` object.
+         * @since 3.0
+         */
+        static toKEXBlockSource(icObj: InnerCoreBlockSource): BlockSource;
+
+        /**
+         * @returns [[BlockSource]] object for the current client
+         * @since 3.0
+         */
+        static getCurrentClientRegion(): Nullable<BlockSource>;
+        
+        /**
+         * @returns [[BlockSource]] object for world generation
+         * @since 3.0
+         */
+        static getCurrentWorldGenRegion(): Nullable<BlockSource>;
+        
+        /**
+         * @returns [[BlockSource]] object of the dimension where the given [[Actor]] is
+         * (null if the given [[Actor]] is not valid or the dimension is not loaded and [[BlockSource]] hasn't been created yet)
+         * @since 3.0
+         */
+        static getDefaultForActor(entity: Actor): Nullable<BlockSource>;
+        
+        /**
+         * @returns [[BlockSource]] object of the dimension where the given entity is
+         * (null if the given entity does not exist or the dimension is not loaded and [[BlockSource]] hasn't been created yet)
+         * @since 3.0
+         */
+        static getDefaultForActor(entityUID: number): Nullable<BlockSource>;
+        
+        /**
+         * @returns [[BlockSource]] object of the given dimension by default
+         * (null if the given dimension is not loaded and [[BlockSource]] hasn't been created yet)
+         * @since 3.0
+         */
+        static getDefaultForDimension(dimensionId: number): Nullable<BlockSource>;
+
+        /**
+         * @returns [[Level]] that owns the dimension of the following [[BlockSource]] or null
+         * if this [[BlockSource]] is not owned by a [[Level]] for some reason
+         * @since 3.0
+         */
+        getLevel(): Nullable<Level>;
+
+        /**
+         * @returns [[Dimension]] that owns the following [[BlockSource]] or null
+         * if this [[BlockSource]] is not owned by a [[Dimension]] for some reason
+         * @since 3.0
+         */
+        getDimensionObject(): Nullable<Dimension>;
+
+    }
+
+}
